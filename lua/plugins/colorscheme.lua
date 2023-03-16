67ce1b74420137de8aff4dc53e8a2075f6e4a018
Max Lefebvre <max.lefebvre@garvin-allen.com>
2023-03-16 14:43:48 -0300

    transparent BG

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- set bg transparent after colorscheme loads
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = augroup("user_colors"),
+  callback = function()
+    vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+  end
+})
+
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", },
 
   -- Configure LazyVim to load gruvbox
   {
