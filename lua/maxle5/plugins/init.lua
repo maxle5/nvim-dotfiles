d798fdd8a4d9864a9cdaada5e2244564ece505b6
Max Lefebvre <max-lefebvre@live.com>
2024-03-18 21:58:33 -0300

    fixed treesitter highlighting

--- lua/maxle5/plugins/init.lua
+++ lua/maxle5/plugins/init.lua
@@ -1,70 +1,70 @@
 return {
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.dashboard")
+    {
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
 
-			-- Set header
-			dashboard.section.header.val = {
-				"                          _        ____   ",
-				"  _ __ ___    __ _ __  __| |  ___ | ___|  ",
-				" | '_ ` _ \\  / _` |\\ \\/ /| | / _ \\|___ \\  ",
-				" | | | | | || (_| | >  < | ||  __/ ___) | ",
-				" |_| |_| |_| \\__,_|/_/\\_\\|_| \\___||____/  ",
-			}
+            -- Set header
+            dashboard.section.header.val = {
+                "                          _        ____   ",
+                "  _ __ ___    __ _ __  __| |  ___ | ___|  ",
+                " | '_ ` _ \\  / _` |\\ \\/ /| | / _ \\|___ \\  ",
+                " | | | | | || (_| | >  < | ||  __/ ___) | ",
+                " |_| |_| |_| \\__,_|/_/\\_\\|_| \\___||____/  ",
+            }
 
-			-- Set menu
-			dashboard.section.buttons.val = {
-				dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-				dashboard.button("f", "  > Find file", ":cd E:/ | Telescope find_files<CR>"),
-				dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-				dashboard.button("q", "󰩈  > Exit", ":q<CR>"),
-			}
+            -- Set menu
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("f", "  > Find file", ":cd E:/ | Telescope find_files<CR>"),
+                dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+                dashboard.button("q", "󰩈  > Exit", ":q<CR>"),
+            }
 
-			-- Send config to alpha
-			alpha.setup(dashboard.opts)
+            -- Send config to alpha
+            alpha.setup(dashboard.opts)
 
-			-- Disable folding on alpha buffer
-			vim.cmd([[
+            -- Disable folding on alpha buffer
+            vim.cmd([[
                 autocmd FileType alpha setlocal nofoldenable
             ]])
-		end,
-	},
+        end,
+    },
 
-	{
-		"ellisonleao/gruvbox.nvim",
-		config = function()
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = false,
-			})
-			vim.cmd("colorscheme gruvbox")
-		end,
-	},
+    {
+        "ellisonleao/gruvbox.nvim",
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+            vim.cmd("colorscheme gruvbox")
+        end,
+    },
 
-	{ "mbbill/undotree" },
+    { "mbbill/undotree" },
 
-	{ "github/copilot.vim" },
+    { "github/copilot.vim" },
 }
--- lua/maxle5/plugins/lsp.lua
+++ lua/maxle5/plugins/lsp.lua
@@ -1,102 +1,101 @@
 local on_attach = function(client, bufnr)
-	vim.print("LSP attached...")
+    vim.print("LSP attached...")
 
-	-- show help in a floating window
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+    -- show help in a floating window
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
 
-	vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
-	vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-	vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
+    vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
 
-	vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0 })
-	vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { buffer = 0 })
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0 })
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { buffer = 0 })
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
 end
 
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"neovim/nvim-lspconfig",
-		"jmederosalvarado/roslyn.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "neovim/nvim-lspconfig",
+        "jmederosalvarado/roslyn.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
 
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"tsserver",
-			},
-		})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "tsserver",
+            },
+        })
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-		cmp.setup({
-			-- completion = {
-			-- 	autocomplete = false,
-			-- },
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<Up>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<Down>"] = cmp.mapping.select_next_item(cmp_select),
-				["<Enter>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
-			}, {
-				{ name = "buffer" },
-			}),
-		})
+        cmp.setup({
+            -- completion = {
+            -- 	autocomplete = false,
+            -- },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<Up>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<Down>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<Enter>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" }, -- For luasnip users.
+            }, {
+                { name = "buffer" },
+            }),
+        })
 
-		-- setup LSPs
-		require("lspconfig").lua_ls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		require("lspconfig").rust_analyzer.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		require("lspconfig").volar.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		require("lspconfig").tsserver.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-		require("roslyn").setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			-- capabilities = <capabilities you would pass to nvim-lspconfig>, -- required
-		})
-	end,
+        -- setup LSPs
+        require("lspconfig").lua_ls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+        require("lspconfig").rust_analyzer.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+        require("lspconfig").volar.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+        require("lspconfig").tsserver.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+        require("roslyn").setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+        })
+    end,
 }
